c9b45652441851d9dd00c0af04cb6ec830ede7eb
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-03-15 11:32:00 +0300

    Update similarity_calculation.py

--- similarity_calculation.py
+++ similarity_calculation.py
@@ -1,6 +1,9 @@
 from sklearn.metrics.pairwise import cosine_similarity
 
-
 def calculate_similarity(query_features, reference_features):
+    # Check dimensions and transpose if necessary
+    if query_features.shape[1] != reference_features.shape[1]:
+        reference_features = reference_features.T
+    
     similarity_scores = cosine_similarity(query_features, reference_features)
     return similarity_scores
